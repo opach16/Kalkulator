e7f891e96a01ba8784106aad0a124e011c708eb0
Konrad Opach <opach16@outlook.com>
2024-06-26 10:58:13 +0200

    Second commit

--- src/main/java/com/kodilla/Calculator.java
+++ src/main/java/com/kodilla/Calculator.java
@@ -10,5 +10,15 @@ public class Calculator {
         return a - b;
     }
 
+    public static void main(String[] args) {
+        Calculator calculator = new Calculator();
+
+        double sum = calculator.add(1.5, 2.9);
+        double diff = calculator.substract(6.4, 1);
+
+        System.out.println("Sum: " + sum);
+        System.out.println("Difference: " + diff);
+    }
+
 
 }
--- src/main/java/com/kodilla/KalkulatorApplication.java
+++ src/main/java/com/kodilla/KalkulatorApplication.java
@@ -9,15 +9,6 @@ public class KalkulatorApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(KalkulatorApplication.class, args);
 
-		Calculator calculator = new Calculator();
-
-		double sum = calculator.add(1.5, 2.9);
-		double diff = calculator.substract(6.4, 1);
-
-		System.out.println("Sum: " + sum);
-		System.out.println("Difference: " + diff);
-
-		System.exit(1);
 	}
 
 }
